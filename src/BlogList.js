c0e040f3896e5c9abcedc5ba947f8029231de842
Chris Laforet <laforet@chrislaforetsoftware.com>
2023-12-16 18:48:08 -0500

    Chapter 11 with raw props

---
+++ src/BlogList.js
@@ -0,0 +1,17 @@
+const BlogList = (props) => {
+    // notice the use of props.blogs to get what was passed
+
+    return (
+       <div className="blog-list">
+           <h2>{props.title}</h2>
+                {props.blogs.map(blog => (
+                    <div className="blog-preview" key={blog.id} >
+                        <h2>{ blog.title }</h2>
+                        <p>Written by { blog.author }</p>
+                    </div>
+                ))}
+       </div>
+    );
+}
+
+export default BlogList;
