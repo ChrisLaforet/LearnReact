305ace2948e577a58b4b58fbc589d4ef3647195b
Chris Laforet <laforet@chrislaforetsoftware.com>
2023-12-17 08:16:05 -0500

    Chapter 15 completed

--- src/Home.js
+++ src/Home.js
@@ -10,6 +10,8 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
 
+    const [name, setName] = useState('mario');
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
@@ -18,12 +20,15 @@ const Home = () => {
     // runs every time there is a re-render (data changes) - do not change state INSIDE effect because this can trigger a loop
     useEffect(() => {
         console.log('use effect ran');
-        console.log(blogs);
-    })
+//        console.log(blogs);
+        console.log(name);
+    }, [name]);
 
   return (
       <div className="home">
           <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+          <button onClick={() => setName('yoshi')}>change name</button>
+          <p>{name}</p>
       </div>
   );
 }
