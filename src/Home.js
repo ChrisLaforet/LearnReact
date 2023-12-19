03eb2b1094fe7a7a85c4a340ca1ed17d8d96f483
Chris Laforet <laforet@chrislaforetsoftware.com>
2023-12-18 22:30:32 -0500

    Chapter 19 completed

--- src/Home.js
+++ src/Home.js
@@ -8,21 +8,31 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     // runs every time there is a re-render (data changes) - do not change state INSIDE effect because this can trigger a loop
     useEffect(() => {
         fetch("http://localhost:8000/blogs")
             .then(response => {
+                if (!response.ok) {
+                    throw Error('Could not fetch the data for that resource');
+                }
                 return response.json();
             })
             .then((data) => {
                 setBlogs(data);
+                setError(null);
+                setIsPending(false);
+            })
+            .catch(err => {
+                setError(err.message);
                 setIsPending(false);
             })
     }, []);
 
   return (
       <div className="home">
+          {error && <div>{ error }</div>}
           {isPending && <div>Loading...</div>}
           {/* we don't want to pass blogs if it is null otherwise we will have an error with blogs.map() in the BlogList */}
           {blogs && <BlogList blogs={blogs} title="All Blogs" />}
