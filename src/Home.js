35f3a641088973bf8db6123539ed79c21bfd69cc
Chris Laforet <laforet@chrislaforetsoftware.com>
2023-12-18 22:16:06 -0500

    Chapter 17 completed

--- src/Home.js
+++ src/Home.js
@@ -6,13 +6,7 @@ const Home = () => {
 
     // Json server startup: npx json-server --watch data/db.json --port 8000
 
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
-
-    const [name, setName] = useState('mario');
+    const [blogs, setBlogs] = useState(null);
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -21,20 +15,19 @@ const Home = () => {
 
     // runs every time there is a re-render (data changes) - do not change state INSIDE effect because this can trigger a loop
     useEffect(() => {
-        console.log('use effect ran');
-//        console.log(blogs);
-        console.log(name);
-    }, [name]);
-
-    // useEffect(() => {
-    //     console.log('Multiple effects')
-    // });
+        fetch("http://localhost:8000/blogs")
+            .then(response => {
+                return response.json();
+            })
+            .then((data) => {
+                setBlogs(data);
+            })
+    }, []);
 
   return (
       <div className="home">
-          <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-          <button onClick={() => setName('yoshi')}>change name</button>
-          <p>{name}</p>
+          {/* we don't want to pass blogs if it is null otherwise we will have an error with blogs.map() in the BlogList */}
+          {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />}
       </div>
   );
 }
