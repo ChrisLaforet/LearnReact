8576d5780da9831bd91b5690cf04202dd8161bc5
Chris Laforet <laforet@chrislaforetsoftware.com>
2023-12-18 22:21:38 -0500

    Chapter 18 completed

--- src/Home.js
+++ src/Home.js
@@ -7,6 +7,7 @@ const Home = () => {
     // Json server startup: npx json-server --watch data/db.json --port 8000
 
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     // runs every time there is a re-render (data changes) - do not change state INSIDE effect because this can trigger a loop
     useEffect(() => {
@@ -16,11 +17,13 @@ const Home = () => {
             })
             .then((data) => {
                 setBlogs(data);
+                setIsPending(false);
             })
     }, []);
 
   return (
       <div className="home">
+          {isPending && <div>Loading...</div>}
           {/* we don't want to pass blogs if it is null otherwise we will have an error with blogs.map() in the BlogList */}
           {blogs && <BlogList blogs={blogs} title="All Blogs" />}
       </div>
