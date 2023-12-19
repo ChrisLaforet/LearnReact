cacf79df1d4cdcc91f9d9aca9f0fdf03aad5174e
Chris Laforet <laforet@chrislaforetsoftware.com>
2023-12-18 23:37:13 -0500

    Chapter 26 completed

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,12 +1,24 @@
 import {useParams} from "react-router-dom";
+import useFetch from "./useFetch";
+import BlogList from "./BlogList";
 
 const BlogDetails = () => {
 
     const { id } = useParams();
 
+    const { isPending, error, data: blog } = useFetch(`http://localhost:8000/blogs/${id}`);
+
     return (
         <div className="blog-details">
-            <h2>Blog details - { id }</h2>
+            {error && <div>{ error }</div>}
+            {isPending && <div>Loading...</div>}
+            {blog && (
+                <article>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by {blog.author}</p>
+                    <div>{blog.body}</div>
+                </article>
+            )}
         </div>
     );
 }
